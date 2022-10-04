930b2ae5eb25fac3abe1c053d818def3a926e6df
Ryan Lee <shapiferoaf@gmail.com>
2022-10-05 02:18:54 +0800

    Basic Routing

--- firstproject/routes/web.php
+++ firstproject/routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route that sends back a view
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Route to users - return string
+Route::get('/users', function () {
+    return 'Welcome to the users page';
+});
+
+//Route to users - Array(JSON) <- auto convert
+Route::get('/users', function () {
+    return ['PHP', 'HTML', 'Laravel'];
+});
+
+//Route to users - JSON object 
+Route::get('/users', function () {
+    return response()->json([
+        'name' => 'Ryan',
+        'course' => 'Laravel Beginners To Advanced'
+    ]);
+});
+
+//Route to users - function
+Route::get('/users', function () {
+    return redirect('/'); //redirects users to homepage
 });
