203b3724277c970e019d9845ffaf3f7da68b29a0
Ryan Lee <shapiferoaf@gmail.com>
2022-10-12 20:00:28 +0800

    Query Builder

--- firstproject/app/Http/Controllers/PostsController.php
+++ firstproject/app/Http/Controllers/PostsController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+        $posts = DB::table('posts')
+            ->whereIn('id', [1, 10])
+            ->get();
+
+        dd($posts);
+        return view('posts.index');
     }
 }
